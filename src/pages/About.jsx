e1df64a2ce7b0bc6bdc420a427651e2001c59cc4
Dawam <muhammadawamali@gmail.com>
2024-08-29 18:58:50 +0700

    feat: Add About page with WidthScreen and Debounce components

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import WidthScreen from '../components/WidthScreen'
+import Debounce from '../components/Debounce'
+
+export default function About() {
+    return (
+        <div>
+            <WidthScreen />
+            <Debounce />
+        </div>
+    )
+}
