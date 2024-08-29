6eac15bdd1123d6b86ebe14525b78cad0c3ce011
Dawam <muhammadawamali@gmail.com>
2024-08-29 18:57:52 +0700

    chore: Add new routes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // import pages
 import Home from './pages/Home'
+import About from './pages/About'
+import Lifecycle from './pages/Lifecycle'
+import Products from './pages/products'
+
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/lifecycle" element={<Lifecycle />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   )
