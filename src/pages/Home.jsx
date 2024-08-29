45d1f80855e55e731b4e3f126598fe8a43de997a
Dawam <muhammadawamali@gmail.com>
2024-08-29 18:57:45 +0700

    chore: Add count state and increment button to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { useState, useEffect } from "react"
 
 export default function Home() {
-    return (
-        <div className='flex items-center justify-center h-screen'>
-            <h1 className='text-3xl font-bold'>Home</h1>
-        </div>
-    )
+
+    const [count, setCount] = useState(0)
+
+    useEffect(() => {
+        console.log('useEffect')
+    }, [count])
+
+  return (
+    <div className="container py-5">
+        <p>{count}</p>
+        <button
+            className="bg-blue-500 hover:bg-blue-700 rounded-lg text-white p-3"
+            onClick={() => {
+                setCount(count + 1)
+            }}
+        >
+            increment
+        </button>
+      
+    </div>
+  )
 }
